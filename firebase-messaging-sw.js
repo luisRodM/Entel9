47808395bd81b1f8a77d5420b4b27f6029c5e5bd
luisRodM <luis.rmanzur@gmail.com>
2023-08-31 15:30:49 -0400

    dfsd

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,16 +17,16 @@ firebase.initializeApp(firebaseConfig)
 // Initialize FCM
 const messaging = firebase.messaging()
 
-// messaging.onBackgroundMessage((payload) => {
-//   console.log(
-//       '[firebase-messaging-sw.js] Received background message ', 
-//       payload
-//       )
-//       console.log(date)
-//   const notificationTitle = payload.notification.title
-//   const notificationOptions = {
-//     body: payload.notification.body
-//   }
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+      '[firebase-messaging-sw.js] Received background message ', 
+      payload
+      )
+      console.log(date)
+  const notificationTitle = payload.notification.title
+  const notificationOptions = {
+    body: payload.notification.body
+  }
 
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// })
+  self.registration.showNotification(notificationTitle, notificationOptions);
+})
